b8e251c768deae38c088bf1eaa7b9ecdc88e644f
Andrew Lingenfelter <andrew.lingenfelter@outlook.com>
2018-09-12 17:39:59 -0400

    Added randomizing features to hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -2,11 +2,27 @@
 // build with:
 // g++ hello.cpp `wx-config --cxxflags` `wx-config --libs`
 
+#include <iostream>
+#include <fstream>
+#include <time.h>
+
+#include "PokemonNames.hpp"
+#include "Moves.hpp"
+#include "Constants.hpp" 
+
 // use precompiled headers if available
 #include <wx/wxprec.h>
 #ifndef WX_PRECOMP
     #include <wx/wx.h>
 #endif
+
+using namespace std;
+
+fstream rom;
+fstream logfile;
+
+void randomize_little_cup();
+void dump_little_cup();
 
 // create our app, based on wxApp
 class MyApp : public wxApp {
@@ -23,6 +39,7 @@ public:
 private:
     // handle menu command events
     void OnHello(wxCommandEvent& event);
+    void OnRandomize(wxCommandEvent& event);
     void OnExit(wxCommandEvent& event);
     void OnAbout(wxCommandEvent& event);
 
@@ -34,12 +51,14 @@ private:
 // These will correspond to menu items.
 // Many built-in events do not need to be defined manually (ex. Exit, Help)
 enum {
-    ID_Hello = 1
+    ID_Hello = 1,
+    ID_Randomize
 };
 
 // create event table
 wxBEGIN_EVENT_TABLE(MyFrame, wxFrame)
     EVT_MENU(ID_Hello, MyFrame::OnHello)
+    EVT_MENU(ID_Randomize, MyFrame::OnRandomize)
     EVT_MENU(wxID_EXIT, MyFrame::OnExit)
     EVT_MENU(wxID_ABOUT, MyFrame::OnAbout)
 wxEND_EVENT_TABLE()
@@ -53,6 +72,21 @@ bool MyApp::OnInit() {
     // create a window frame and display it
     MyFrame *frame = new MyFrame("Test Program",wxPoint(50,50),wxSize(640,480) );
     frame->Show(true);
+
+    // open the ROM
+    rom.open("rom.z64", ios::in | ios::out | ios::binary);
+    if (!rom.is_open()) {
+        cout << "Failed to open ROM" << endl;
+        return 1;
+    }
+
+    // open log file
+    logfile.open("log.txt", ios::out);
+    if (!logfile.is_open()) {
+        cout << "Failed to open log file" << endl;
+        return 2;
+    }
+
     return true;
 }
 
@@ -64,6 +98,7 @@ MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
     wxMenu *menuFile = new wxMenu;
     menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
         "Help string show in status bar for this menu item");
+    menuFile->Append(ID_Randomize,"&Randomize","Randomize the ROM");
     menuFile->AppendSeparator();
     menuFile->Append(wxID_EXIT);
 
@@ -85,6 +120,8 @@ MyFrame::MyFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
 }
 
 void MyFrame::OnExit(wxCommandEvent& event) {
+    // close 
+    rom.close();
     Close(true);
 }
 
@@ -95,4 +132,64 @@ void MyFrame::OnAbout(wxCommandEvent& event) {
 
 void MyFrame::OnHello(wxCommandEvent& event) {
     wxLogMessage("Hello");
+}
+
+void MyFrame::OnRandomize(wxCommandEvent& event) {
+    randomize_little_cup();
+    dump_little_cup();
+}
+
+void randomize_little_cup() {
+
+    // seed the random number generator
+    srand(time(NULL));
+
+    for (int j = 0; j < LITTLE_CUP_COUNT; j++ ) { 
+
+        // go to pokemon
+        rom.seekg(LITTLE_CUP_RENTALS + (SIZE_OF_POKEMON*j) + MOVE1);
+
+        // for each move
+        for (int i = 0; i < 4; i++) { 
+
+            // randomly pick a move and insert it
+            uint8_t move = (rand() % MOVES_COUNT) + 1;
+            rom << move;
+        }
+
+    }
+}
+
+void dump_little_cup() {
+    // read and print level and species
+    uint8_t level = 0;
+    uint8_t species = 0;
+
+    logfile << "LITTLE CUP RENTALS" << endl
+        << "------------------" << endl;
+
+    // go to first LittleCup Pokemon
+    // rom.seekg(LITTLE_CUP_RENTALS);
+    for (int i = 0; i < LITTLE_CUP_COUNT;  i++) {
+
+        // seek to pokemon
+        rom.seekg(LITTLE_CUP_RENTALS + (SIZE_OF_POKEMON * i));
+        rom >> level;
+        rom >> species;
+
+        // write level and name
+        logfile << "Level " << (int)level << " " 
+            << (int)species << ":" << PokemonNames[species] <<  endl;
+        
+        // write out moveset
+        rom.seekg(LITTLE_CUP_RENTALS + (SIZE_OF_POKEMON * i) + MOVE1);
+        logfile << "\t";
+        for (int j = 0; j < 4; j++) {
+            uint8_t move = 0;
+            rom >> move;
+            logfile << Moves[move];
+            if (j < 3) { logfile << " / "; }
+        }
+        logfile << endl;
+    }
 }
